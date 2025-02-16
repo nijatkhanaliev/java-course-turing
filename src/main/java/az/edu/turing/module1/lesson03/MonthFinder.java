a4b5acacfd7cdcfe205e69c49574ca15ec2c08c8
nijat <khanalievn@gmail.com>
2025-02-16 19:29:27 +0400

    MaximumFinder attached

--- src/main/java/az/edu/turing/module1/lesson03/MonthFinder.java
+++ src/main/java/az/edu/turing/module1/lesson03/MonthFinder.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class MonthFinder {
 
     public static void main(String[] args) {
-
+        
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a number:");
         int input = scanner.nextInt();
---
+++ src/main/java/az/edu/turing/module1/lesson04/MaximumFinder.java
@@ -0,0 +1,30 @@
+package az.edu.turing.module1.lesson04;
+
+import java.util.Scanner;
+
+public class MaximumFinder {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a length");
+
+        int length = sc.nextInt();
+        int[] arr = new int[length];
+
+        for(int i=0;i<length;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        int max=arr[0];
+        cd 
+        for(int i=0;i<length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println(max);
+
+    }
+
+}
