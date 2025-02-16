6e2e803176ed2c1894fc7a29539f2b1e4c48bb0c
nijat <khanalievn@gmail.com>
2025-02-16 19:36:48 +0400

    MaximumFinder attached

--- src/main/java/az/edu/turing/module1/lesson04/MaximumFinder.java
+++ src/main/java/az/edu/turing/module1/lesson04/MaximumFinder.java
@@ -17,8 +17,8 @@ public class MaximumFinder {
         }
 
         int max = arr[0];
-        
-        for (int i = 0; i < length; i++) {
+
+        for (int i = 1; i < length; i++) {
             if (arr[i] > max) {
                 max = arr[i];
             }
