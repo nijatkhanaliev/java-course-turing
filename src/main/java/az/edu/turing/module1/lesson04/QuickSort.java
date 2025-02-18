8c1f666441e0f74b0feee1159b4c8c9b7589016f
nijat <khanalievn@gmail.com>
2025-02-18 23:33:28 +0400

    QuickSort.java attached

--- src/main/java/az/edu/turing/module1/lesson04/QuickSort.java
+++ src/main/java/az/edu/turing/module1/lesson04/QuickSort.java
@@ -1,2 +1,49 @@
-package az.edu.turing.module1.lesson04;public class QuickSort {
+package az.edu.turing.module1.lesson04;
+
+import java.util.Arrays;
+
+public class QuickSort {
+
+    public static void main(String[] args) {
+
+        int[] arr = {2, 7, 12, 9, 7, 4, 3, 6};
+        quickSort(arr, 0, arr.length - 1);
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    private static void quickSort(int[] arr, int start, int end) {
+
+        if (end <= start) return;
+
+        int pivot = sortByPivot(arr, start, end);
+        quickSort(arr, start, pivot - 1);
+        quickSort(arr, pivot + 1, end);
+
+    }
+
+    private static int sortByPivot(int[] arr, int start, int end) {
+        int i = start - 1;
+        int pivot = arr[end];
+
+        for (int j = start; j < end; j++) {
+
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+
+        }
+
+        i++;
+        int temp = arr[i];
+        arr[i] = arr[end];
+        arr[end] = temp;
+
+        return i;
+    }
+
 }
