414bfe5aa0fdc7e5fcf84e3347479aa5c3cbc407
nijat <khanalievn@gmail.com>
2025-02-27 00:02:04 +0400

    WeekPlanner attached

--- src/main/java/az/edu/turing/module1/lesson06/ShootingGame.java
+++ src/main/java/az/edu/turing/module1/lesson06/ShootingGame.java
@@ -1,2 +1,124 @@
-package az.edu.turing.module1.lesson06;public class ShootingGame {
+package az.edu.turing.module1.lesson06;
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class ShootingGame {
+
+    static Random random = new Random();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] targetArr = createTargetArr();
+        char[][] gameBoard = createBoard();
+        int attempts = 0;
+
+        for (int[] arr : targetArr) {
+            for (int a : arr) {
+                System.out.print(a + " ");
+            }
+            System.out.println();
+        }
+
+        printBoard(gameBoard);
+        System.out.println("All set. Get ready to rumble");
+        while (attempts < 3) {
+            System.out.println("Enter a row and column: ");
+            if (!isInputValid(sc)) {
+                sc.nextLine();
+                continue;
+            }
+            int row = sc.nextInt();
+
+            if (!isInputValid(sc)) {
+                sc.nextLine();
+                continue;
+            }
+            int col = sc.nextInt();
+
+            if (row >= 5 || row < 0 || col >= 5 || col < 0) {
+                System.out.println("Invalid input.Enter again: ");
+                continue;
+            }
+
+            if (isExist(row, col, targetArr, targetArr.length) && gameBoard[row][col] != 'x') {
+                System.out.println("You have won!");
+                gameBoard[row][col] = 'x';
+                attempts++;
+            } else if (gameBoard[row][col] == 'x') {
+                System.out.println("You shot same place");
+            } else {
+                gameBoard[row][col] = '*';
+            }
+
+            if (attempts != 3) printBoard(gameBoard);
+        }
+
+        System.out.println("Congratulations!");
+        printBoard(gameBoard);
+    }
+
+    public static int[][] createTargetArr() {
+        int[][] targetArr = new int[3][2];
+        int row;
+        int col;
+
+        for (int i = 0; i < targetArr.length; i++) {
+            row = random.nextInt(5);
+            col = random.nextInt(5);
+            while (isExist(row, col, targetArr, i)) {
+                row = random.nextInt(5);
+                col = random.nextInt(5);
+            }
+
+            targetArr[i][0] = row;
+            targetArr[i][1] = col;
+        }
+
+        return targetArr;
+    }
+
+    public static boolean isExist(int row, int col, int[][] targetArr, int limit) {
+        boolean isExist = false;
+        for (int i = 0; i < limit; i++) {
+            if (row == targetArr[i][0] && col == targetArr[i][1]) {
+                isExist = true;
+                break;
+            }
+        }
+
+        return isExist;
+    }
+
+    public static boolean isInputValid(Scanner sc) {
+        if (!sc.hasNextInt()) {
+            System.out.println("Invalid input.Enter again: ");
+
+            return false;
+        }
+
+        return true;
+    }
+
+    public static char[][] createBoard() {
+        char[][] board = new char[5][5];
+        for (char[] chars : board) {
+            for (int i = 0; i < chars.length; i++) {
+                chars[i] = '-';
+            }
+        }
+
+        return board;
+    }
+
+    public static void printBoard(char[][] board) {
+        for (char[] chars : board) {
+            for (char ch : chars) {
+                System.out.print(ch);
+            }
+            System.out.println();
+        }
+    }
+
 }
--- src/main/java/az/edu/turing/module1/lesson06/WeekPlanner.java
+++ src/main/java/az/edu/turing/module1/lesson06/WeekPlanner.java
@@ -1,2 +1,78 @@
-package az.edu.turing.module1.lesson06;public class WeekPlanner {
+package az.edu.turing.module1.lesson06;
+
+import java.util.Scanner;
+
+public class WeekPlanner {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[][] schedule = createSchedule();
+        boolean isExit = false;
+
+        while (!isExit) {
+            System.out.println("Enter day of week");
+            String day = sc.nextLine().trim();
+
+            if (day.startsWith("change")) {
+                day = day.substring(7).trim();
+                if (isDayExist(day, schedule) == null) {
+                    System.out.println("Sorry, i do not understand yoy. Please try again.");
+                    continue;
+                }
+                System.out.println("Enter new task for " + day);
+                String task = sc.nextLine().trim();
+                changeTask(day, task, schedule);
+
+                continue;
+            }
+
+            if (day.equalsIgnoreCase("exit")) {
+                isExit = true;
+                continue;
+            }
+
+            String task = isDayExist(day.toLowerCase(), schedule);
+            System.out.println(task == null ? "Sorry,i do not understand you. Please try again." : task);
+        }
+        sc.close();
+    }
+
+    public static void changeTask(String day, String newTask, String[][] schedule) {
+        for (String[] sc : schedule) {
+            if (day.equalsIgnoreCase(sc[0])) {
+                sc[1] = newTask;
+            }
+        }
+    }
+
+    public static String isDayExist(String day, String[][] schedule) {
+        for (String[] sc : schedule) {
+            if (day.equalsIgnoreCase(sc[0])) {
+                return sc[1];
+            }
+        }
+
+        return null;
+    }
+
+    public static String[][] createSchedule() {
+        String[][] schedule = new String[7][2];
+        schedule[0][0] = "Sunday";
+        schedule[0][1] = "do home work";
+        schedule[1][0] = "Monday";
+        schedule[1][1] = "go to courses; watch a film";
+        schedule[2][0] = "Tuesday";
+        schedule[2][1] = "read a book";
+        schedule[3][0] = "Wednesday";
+        schedule[3][1] = "do exercise";
+        schedule[4][0] = "Thursday";
+        schedule[4][1] = "go shopping";
+        schedule[5][0] = "Friday";
+        schedule[5][1] = "play football";
+        schedule[6][0] = "Saturday";
+        schedule[6][1] = "play guitar";
+
+        return schedule;
+    }
+
 }
