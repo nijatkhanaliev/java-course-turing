bf06cc54acdba66f47a37f6fb676fa71622b088c
nijat <khanalievn@gmail.com>
2025-03-10 00:34:09 +0400

    java-course-turing

--- src/main/java/az/edu/turing/module2/universitydemo/Course.java
+++
@@ -1,8 +0,0 @@
-package az.edu.turing.module2.universitydemo;
-
-public class Course {
-    private String name;
-    private double grade;
-    private Student[] students;
-
-}
--- src/main/java/az/edu/turing/module2/universitydemo/University.java
+++
@@ -1,4 +0,0 @@
-package az.edu.turing.module2.universitydemo;
-
-public class University {
-}
