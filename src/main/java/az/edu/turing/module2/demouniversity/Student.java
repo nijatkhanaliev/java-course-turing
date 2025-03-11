3ce86205b69938fae3f36673b1918d751283bf8a
nijat <khanalievn@gmail.com>
2025-03-11 23:55:44 +0400

    Student can enroll course and calculate gpa,update grade

--- src/main/java/az/edu/turing/module2/demouniversity/Student.java
+++ src/main/java/az/edu/turing/module2/demouniversity/Student.java
@@ -1,14 +1,13 @@
 package az.edu.turing.module2.demouniversity;
 
 import java.util.Arrays;
-import java.util.Objects;
 
 public class Student extends Person {
     private String major;
     private Course[] courses;
     private double[] grades;
 
-    public Student(String major,int limit) {
+    public Student(String major, int limit) {
         this.major = major;
         this.courses = new Course[limit];
         this.grades = new double[limit];
@@ -36,6 +35,48 @@ public class Student extends Person {
 
     public void setGrades(double[] grades) {
         this.grades = grades;
+    }
+
+    private int count;
+
+    public void enrollCourse(Course course) {
+        for (Course cr : courses) {
+            if (cr.getCourseCode() == course.getCourseCode()) {
+                System.out.println("Course has already enrolled ");
+                return;
+            }
+        }
+        courses[count] = course;
+        count++;
+    }
+
+    public void updateGrade(int courseCode, double newGrade) {
+        for (int i = 0; i < courses.length; i++) {
+            if (courses[i].getCourseCode() == courseCode) {
+                grades[i] = newGrade;
+                return;
+            }
+        }
+        System.out.println("Course does not exists");
+    }
+
+    public void updateGrade(String courseName, double newGrade) {
+        for (int i = 0; i < courses.length; i++) {
+            if (courses[i].getCourseName().equalsIgnoreCase(courseName)) {
+                grades[i] = newGrade;
+                return;
+            }
+        }
+        System.out.println("Course does not exists");
+    }
+
+    public double calculateStudentGpa() {
+        double sum = 0;
+        for (double grade : grades) {
+            sum += grade;
+        }
+
+        return (sum / grades.length);
     }
 
     @Override
