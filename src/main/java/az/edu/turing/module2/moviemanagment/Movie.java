9be7ce97639c8416171efa447cce0fd672c05fb4
nijat <khanalievn@gmail.com>
2025-03-29 12:04:12 +0400

    Movie.java attached

---
+++ src/main/java/az/edu/turing/module2/moviemanagment/Movie.java
@@ -0,0 +1,38 @@
+package az.edu.turing.module2.moviemanagment;
+
+public class Movie {
+    private String name;
+    private double rating;
+
+    public Movie(String name, double rating) {
+        this.name = name;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        if (rating < 0.0 || rating > 10.0) {
+            System.out.println("Rating cannot be more than 10.0 or less than 0.0");
+            return;
+        }
+
+        this.rating = rating;
+    }
+
+    @Override
+    public String toString() {
+        return "name='" + name + "  " +
+                "rating=" + rating;
+    }
+}
