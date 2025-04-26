2e7b096f81add27b0b1e3f51d1734d178f850642
nijat <khanalievn@gmail.com>
2025-04-26 18:06:35 +0400

    stream api

--- src/main/java/az/edu/turing/module3/lesson05/Main.java
+++ src/main/java/az/edu/turing/module3/lesson05/Main.java
@@ -7,12 +7,12 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
 
-        List<User> users = List.of(new User(1, "nicat"), new User(2, "saiq"),
+        List<User> users = List.of(new User(1, "nicat"), new User(1, "saiq"),
                 new User(3, "rasul"), new User(4, "malik"));
 
 
         Map<Integer, User> map = users.stream()
-                .collect(Collectors.toMap(User::getId, user -> user));
+                .collect(Collectors.toMap(User::getId, user -> user,(user1,user2)->user1));
 
         System.out.println(map);
 
